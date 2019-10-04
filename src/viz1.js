b852fc9db6ea6aea43179c6b0a17631080bf75d7
Santi Scalzadonna <pilujander@gmail.com>
2019-10-04 10:41:00 -0300

    Ported viz1 from observable

--- src/viz1.js
+++ src/viz1.js
@@ -3,8 +3,8 @@
 //https://gist.githubusercontent.com/scalzadonna/62b30e39cc42b40c94c238d32e8644c3/raw/8d529ef0c268d3a747b05486314bba18844c9896/balkan-participations.csv	
 
 const minCollabs = 3;
-const width = 640;
-const height = 640;
+const width = 540;
+const height = 540;
 d3.csv("https://gist.githubusercontent.com/scalzadonna/62b30e39cc42b40c94c238d32e8644c3/raw/8d529ef0c268d3a747b05486314bba18844c9896/balkan-participations.csv", function (d) {
     return {
         activityId: d.riactivityId,
@@ -18,10 +18,8 @@ d3.csv("https://gist.githubusercontent.com/scalzadonna/62b30e39cc42b40c94c238d32
         activitiesById = _.groupBy(participantsData, 'activityId');
         collabs = _.filter(activitiesById, function (parts) { return parts.length >= minCollabs });
         results = extractRelationships(collabs);
-        console.log(color);
-        let graph = draw(results[1], results[0], color, drag);
+        let graph = draw(results[1], results[0]);
         document.getElementById("viz1").appendChild(graph);
-        console.log('drawed');
     });
 
 const extractRelationships = function (collabsGroup) {
@@ -90,15 +88,47 @@ const simulation = function () {
 };
 
 
-const draw = function (linksJson, participantsJson, color, drag) {
+const draw = function (linksJson, participantsJson) {
 
     const links = linksJson.map(d => Object.create(d));
     const nodes = participantsJson.map(d => Object.create(d));
+    const scale = d3.scaleOrdinal(d3.schemeCategory10);
+    const drag = function () {
+
+        return (
+            simulation => {
+
+                function dragstarted(d) {
+                    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+                    d.fx = d.x;
+                    d.fy = d.y;
+                }
+
+                function dragged(d) {
+                    d.fx = d3.event.x;
+                    d.fy = d3.event.y;
+                }
+
+                function dragended(d) {
+                    if (!d3.event.active) simulation.alphaTarget(0);
+                    d.fx = null;
+                    d.fy = null;
+                }
+
+                return d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended);
+            }
+        )
+
+    }
+
     const simulation = d3.forceSimulation(nodes)
         .force("link", d3.forceLink(links).id(d => d.id))
         .force("charge", d3.forceManyBody())
-        .force("x", d3.forceX().strength(0.05))
-        .force("y", d3.forceY().strength(0.08));
+        .force("x", d3.forceX().strength(0.15))
+        .force("y", d3.forceY().strength(0.15));
 
     const svg = d3.create("svg")
         .attr("viewBox", [-width / 2, -height / 2, width, height]);
@@ -118,7 +148,7 @@ const draw = function (linksJson, participantsJson, color, drag) {
         .data(nodes)
         .join("circle")
         .attr("r", (d) => 3 + d.count)
-        .attr("fill", color)
+        .attr("fill", d => scale(d.group))
         .call(drag(simulation));
 
     node.append("title")
@@ -138,41 +168,5 @@ const draw = function (linksJson, participantsJson, color, drag) {
 
 
     return svg.node();
-}
-
-const color = function () {
-    const scale = d3.scaleOrdinal(d3.schemeCategory10);
-    return d => scale(d.group);
-};
-
-const drag = function () {
-
-    return (
-        simulation => {
-
-            function dragstarted(d) {
-                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-                d.fx = d.x;
-                d.fy = d.y;
-            }
-
-            function dragged(d) {
-                d.fx = d3.event.x;
-                d.fy = d3.event.y;
-            }
-
-            function dragended(d) {
-                if (!d3.event.active) simulation.alphaTarget(0);
-                d.fx = null;
-                d.fy = null;
-            }
-
-            return d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended);
-        }
-    )
-
 }
 
